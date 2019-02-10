892b7c34e87b42d7758143b3ed889a456fc94f29
RaviTeja <holyindus08@gmail.com>
2019-02-10 08:19:15 +0530

    Added feature to ignore duplicates

--- start.py
+++ start.py
@@ -8,24 +8,24 @@ updater = Updater(token = os.environ['token'])
 def start(bot,update):
     bot.send_message(chat_id=update.message.chat_id,text="I'am a bot created by user @RaviTeja51")
     #to store the user name and chat_id in a text file
+    with open("chat_id.txt","r") as f:
+        already_existing = f.readlines()
+        print(already_existing)
 
-    with open("chat_id.txt","a+") as f:
+    with open("chat_id.txt","a") as f:
         try:
-            already_existing = f.readlines()
+            c = 0
             name = update.message.from_user.first_name
             id = update.message.chat_id
-            if (str(name)+ " : "+ str(id)) not in already_existing:
+            #to avoid duplicates
+            for i in already_existing:
+                if (str(name)+ " : "+ str(id) + "\n")==i:
+                    c = 1
+
+            if not c:
                 f.write(str(name)+" : "+str(id))
         except:
             print("failed")
-
-
-
-
-
-
-
-
 
 dispatcher = updater.dispatcher
 start_handler = CommandHandler('start',start)
